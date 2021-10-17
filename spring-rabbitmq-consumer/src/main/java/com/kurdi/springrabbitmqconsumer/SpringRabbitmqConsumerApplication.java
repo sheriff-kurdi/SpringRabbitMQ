466426f8fab7d379579dbada58aba274ef1eb737
sheriff-kurdi <sheriff.kurdi@gmail.com>
2021-10-17 15:28:09 +0200

    Initial commit

---
+++ spring-rabbitmq-consumer/src/main/java/com/kurdi/springrabbitmqconsumer/SpringRabbitmqConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.kurdi.springrabbitmqconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitmqConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabbitmqConsumerApplication.class, args);
+	}
+
+}
